059103596129cf721023405e7ef0cd5b2a5c78dc
Iamiran1 <vinicius.c.miranda@icloud.com>
2025-04-10 16:17:25 -0300

    test

--- exe3/main.c
+++ exe3/main.c
@@ -1,41 +1,44 @@
-// #include <FreeRTOS.h>
-// #include <task.h>
-// #include <semphr.h>
-// #include <queue.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include <semphr.h>
+#include <queue.h>
 
-// #include "pico/stdlib.h"
-// #include <stdio.h>
+#include "pico/stdlib.h"
+#include <stdio.h>
 
-// #include "hardware/gpio.h"
-// #include "hardware/i2c.h"
-// #include "mpu6050.h"
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
+#include "mpu6050.h"
 
-// const int I2C_CHIP_ADDRESS = 0x68;
-// const int I2C_SDA_GPIO = 20;
-// const int I2C_SCL_GPIO = 21;
+const int I2C_CHIP_ADDRESS = 0x68;
+const int I2C_SDA_GPIO = 20;
+const int I2C_SCL_GPIO = 21;
 
-// void i2c_task(void *p) {
-//     i2c_init(i2c_default, 400 * 1000);
-//     gpio_set_function(I2C_SDA_GPIO, GPIO_FUNC_I2C);
-//     gpio_set_function(I2C_SCL_GPIO, GPIO_FUNC_I2C);
-//     gpio_pull_up(I2C_SDA_GPIO);
-//     gpio_pull_up(I2C_SCL_GPIO);
+void i2c_task(void *p) {
+    i2c_init(i2c_default, 400 * 1000);
+    gpio_set_function(I2C_SDA_GPIO, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL_GPIO, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA_GPIO);
+    gpio_pull_up(I2C_SCL_GPIO);
 
-//     // TODO
-//     // read id chip BMP280
-//     printf("BMP280 ID: 0x%X \n", buffer[0]);
+    // TODO
+    // read id chip BMP280
+    uint8_t buffer[6];
 
-//     while (1) {
-//         vTaskDelay(pdMS_TO_TICKS(200));
-//     }
-// }
+    i2c_read_blocking(i2c_default, I2C_CHIP_ADDRESS, buffer, 1, false);
+    printf("BMP280 ID: 0x%X \n", buffer[0]);
 
-// int main() {
-//     stdio_init_all();
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+}
 
-//     xTaskCreate(i2c_task, "i2c task", 4095, NULL, 1, NULL);
-//     vTaskStartScheduler();
+int main() {
+    stdio_init_all();
 
-//     while (true) {
-//     }
-// }
+    xTaskCreate(i2c_task, "i2c task", 4095, NULL, 1, NULL);
+    vTaskStartScheduler();
+
+    while (true) {
+    }
+}
